0ba7deaa285ad5658d37a60bec938f134d878e8f
muhsiltomsher <muhsil@tomsher.com>
2025-03-10 12:03:02 +0400

    a

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,36 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
     build: {
-        outDir: 'dist', // Change to 'public' if needed
+        outDir: 'dist',  // Ensure this is set
         emptyOutDir: true,
     }
 });
+
+
+
+
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/css/app.css',
+//                 'resources/js/app.js',
+//             ],
+//             refresh: true,
+//         }),
+//     ],
+//     build: {
+//         outDir: 'dist',
+//         emptyOutDir: true,
+//     }
+// });
